f4c7f1a23c77c103d51e60c6ffe178314863a3b7
pierrick pobelle <pierrick.pobelle@gmail.com>
2016-05-28 00:07:14 +0200

    only moderator can /next

--- liveChatMusic/livecoding.js
+++ liveChatMusic/livecoding.js
@@ -1,41 +1,34 @@
 var textarea = $('#message-textarea');
 var submit = $('input[type="submit"]');
+
 function postMessage(message) {
     $("textarea").val(message);
     submit.trigger('click');
 }
 
-  $(".message").addClass("read");
-  setInterval(function(){
-  	$(".message:not(.read)").each(function(){
-  		$(this).addClass("read");
-  		var text = $(this).text();
-  		// if(text != undefined){
-	  	// 	if(text.indexOf(' joined the room.') != -1) {
-	   //          var userJoined = text.slice(0, text.indexOf(' joined the room.'));
-	   //          postMessage("Bonsoir " + userJoined + " bienvenue sur mon stream! "+
-    //             "Des commandes sont disponibles pour la musique : "+"/next et /prev");
-    //     	}   
-    // 	}
-  		switch($(this).children("a")[0].nextSibling.nodeValue ){
-  			case "/next":
-  			  opener.postMessage("next","https://play.google.com");
-  			break;
-  			case "/prev":
-  			  	opener.postMessage("prev","https://play.google.com");
-  			break;
-  		}
-  		
-  	});
-  },1000)
+$(".message").addClass("read");
+  setInterval(function() {
+    $(".message:not(.read)").each(function() {
+        $(this).addClass("read");
+        if($(this).children("a").is(".role-moderator")){
+          switch ($(this).children("a")[0].nextSibling.nodeValue) {
+              case "/next":
+                  opener.postMessage("next", "https://play.google.com");
+                  break;
+              case "/prev":
+                  opener.postMessage("prev", "https://play.google.com");
+                  break;
+          }
+        }
+    });
+}, 1000);
 
-
-  function receiveMessage(event)
-{
+function receiveMessage(event) {
     if (event.origin !== "https://play.google.com")
-      return;
-    
-    postMessage("Musique changée pour : "+ event.data);
+        return;
+
+    postMessage("Musique changée pour : " + event.data);
 }
 
-window.addEventListener("message", receiveMessage, false);alert("Chargé !");
+window.addEventListener("message", receiveMessage, false);
+alert("Chargé !");
